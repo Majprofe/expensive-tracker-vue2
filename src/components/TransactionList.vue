6d0c18f668ef5d3aeccdcb192726dd81c37c74e3
majprofe <marrjua393@g.educaand.es>
2024-01-25 17:25:17 +0100

    Hasta transactionlist

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,37 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li 
+        v-for="transaction in transactions" 
+        :key="transaction.id" 
+        :class="transaction.amount < 0 ? 'minus' : 'plus'">
+        {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn">x</button>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 299.97 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+])
+
+/* export default {
+    setup() {
+        const transactions = [
+            { id: 1, text: 'Flower', amount: -19.99 },
+            { id: 2, text: 'Salary', amount: 299.97 },
+            { id: 3, text: 'Book', amount: -10 },
+            { id: 4, text: 'Camera', amount: 150 }
+        ]
+        return {
+            transactions,
+        }
+    }
+} */
+</script>
+
+<style scoped></style>
